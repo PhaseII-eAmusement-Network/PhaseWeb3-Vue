a660c06987167184c4ba7fa37c5ae44cc945ae59
archibun <285294811+archibun@users.noreply.github.com>
2026-08-02 22:00:37 -0700

    ddr: full appeal card preview

--- src/components/Cards/DdrAppealCardBox.vue
+++ src/components/Cards/DdrAppealCardBox.vue
@@ -35,6 +35,7 @@ const loading = ref(false);
 const optionForm = ref({});
 
 const APPEAL_ID = "customize.1_0";
+const appealId = ref(APPEAL_ID);
 
 loadCustomizations();
 function loadCustomizations() {
@@ -85,6 +86,21 @@ function revertCustomizations() {
 
   isModified.value = false;
 }
+
+// TODO: switch to spritesheet/actual font?
+function getLetterUrl(type, file) {
+  const ASSET_PATH = import.meta.env.VITE_GAME_ASSET_PATH;
+  return `${ASSET_PATH}/font/ddr/${version.value}/${type}/${file}.png`;
+}
+const replacements = new Map([
+  [" ", "blank"],
+  ["&", "ampersand"],
+  ["-", "hyphen"],
+  ["?", "question"],
+  ["!", "exclamation"],
+  [".", "period"],
+  ["$", "dollar"],
+]);
 </script>
 
 <template>
@@ -108,23 +124,87 @@ function revertCustomizations() {
           />
         </FormField>
       </form>
-      <div class="place-self-center">
+
+      <div
+        class="flex flex-col items-center justify-center w-full max-w-111 justify-self-center"
+      >
         <label class="block mb-2 text-center">
           Previewing:
           <strong class="font-bold">Appeal Board</strong>
         </label>
-        <img
-          :src="getUrl('appeal_login')"
-          class="object-contain"
-          :width="444"
-          :height="200"
-        />
-        <img
-          :src="getUrl('appeal')"
-          class="pt-4 object-contain"
-          :width="444"
-          :height="58"
-        />
+
+        <div class="relative w-full aspect-444/58">
+          <img
+            :src="getUrl('appeal_login')"
+            class="absolute inset-0 w-full h-full object-contain"
+          />
+
+          <!-- area label + shadow -->
+          <img
+            :src="getLetterUrl('area', profile.area)"
+            class="absolute object-right object-contain bottom-[14%] right-[1.75%] scale-80 z-10"
+          />
+          <img
+            :src="getLetterUrl('area', profile.area)"
+            class="absolute object-right object-contain bottom-[11%] right-[1.25%] scale-80 brightness-0"
+          />
+
+          <!-- dancer name -->
+          <div
+            class="relative h-full"
+            :style="{
+              marginLeft:
+                (getNestedValue(optionForm, appealId) ?? 0) >= 100000
+                  ? '30.25%'
+                  : '3.6%',
+            }"
+          >
+            <div
+              v-for="layer in [
+                'bottom-[18%] left-[0.7%] brightness-0',
+                'bottom-[20.75%] left-[0.3%] z-10',
+              ]"
+              :class="[
+                'w-full absolute flex items-center justify-start h-[33.5%]',
+                layer,
+              ]"
+            >
+              <img
+                v-for="character of profile.username.split('')"
+                class="object-contain h-full ml-[-0.175%]"
+                :src="
+                  getLetterUrl(
+                    'entry',
+                    replacements.get(character) ?? character.toLowerCase(),
+                  )
+                "
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="relative w-full aspect-444/200 mt-1">
+          <img
+            :src="getUrl('appeal')"
+            class="absolute inset-0 w-full h-full object-contain"
+          />
+
+          <!-- dancer name -->
+          <div
+            class="w-full h-[28%] absolute flex items-center justify-center bottom-2 left-0"
+          >
+            <img
+              v-for="character of profile.username.split('')"
+              class="object-contain h-[65.5%]"
+              :src="
+                getLetterUrl(
+                  'result',
+                  replacements.get(character) ?? character.toLowerCase(),
+                )
+              "
+            />
+          </div>
+        </div>
       </div>
     </div>
 
